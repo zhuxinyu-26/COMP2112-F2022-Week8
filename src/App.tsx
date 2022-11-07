3d0ea1f879eff425d4c269e19a861a14859e0d7f
Xinyu Zhu <zhuxinyu_26@163.com>
2022-11-07 00:03:40 -0500

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+
+// common components
+import Header from './Views/components/header';
 // Styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
+// JS Libraries
+import 'jquery/dist/jquery.min.js';
+import '@popperjs/core/dist/umd/popper.min.js';
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       
       <h1 className='container'>Hello, World!</h1>
     </div>
